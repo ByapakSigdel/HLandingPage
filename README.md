1b21a7343cdb782aa742fe52359c8e3c4157d6ce
ByapakSigdel <sigdelmb123@gmail.com>
2024-09-11 18:25:00 +0545

    Finished the our works section

--- README.md
+++ README.md
@@ -17,3 +17,9 @@ npm run dev
 ```
 
 Now you can visit https://localhost:3000 in your browser.
+
+
+To DO:
+Add a animation in the hero section.
+Add a hover section over the our team section
+Redefien the card in the our work section to animate it properly
--- components/features3/index.tsx
+++ components/features3/index.tsx
@@ -1,10 +1,14 @@
+/* eslint-disable react/no-unescaped-entities */
 import {Card, Divider, Text} from '@nextui-org/react';
-import React from 'react';
+import React, { useState } from 'react';
 import {BoxIcon} from '../icons/BoxIcon';
 import {Box} from '../styles/box';
 import {Flex} from '../styles/flex';
+import { TiArrowSortedUp } from 'react-icons/ti'; // Adjust based on your icon library
+import { FaGithub } from 'react-icons/fa'; // Adjust based on your icon library
 
 export const Features3 = () => {
+   const [isHovered, setIsHovered] = useState(false);
    return (
       <>
          <Box
@@ -22,9 +26,9 @@ export const Features3 = () => {
                }}
             >
                <Text span css={{color: '$blue600'}}>
-                  Awesome Feature
+                  Checkout some of our works
                </Text>
-               <Text h3>Your title here</Text>
+               <Text h3>Our Works</Text>
                <Text
                   span
                   css={{
@@ -33,9 +37,7 @@ export const Features3 = () => {
                      textAlign: 'center',
                   }}
                >
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                  condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl,
-                  nec
+                  At Hundred Studios, we create tailored digital solutions, including websites, apps, games, and custom software, while exploring new technologies to stay ahead.
                </Text>
             </Flex>
             <Flex
@@ -52,11 +54,9 @@ export const Features3 = () => {
                      <Flex css={{gap: '0.5rem'}}>
                         <BoxIcon />
                         <Flex direction={'column'}>
-                           <Text h5>Your Title</Text>
+                           <Text h5>Word Clash</Text>
                            <Text span>
-                              Lorem ipsum dolor sit amet, consectetur adipiscing
-                              elit. Sed condimentum, nisl ut aliquam lacinia,
-                              nisl nisl aliquet nisl, nec
+                              Word Clash is a fun, multiplayer based word guessing game in which within a party you have to guess the word containing the letters provided by the game.
                            </Text>
                         </Flex>
                      </Flex>
@@ -67,11 +67,9 @@ export const Features3 = () => {
                      <Flex css={{gap: '0.5rem'}}>
                         <BoxIcon />
                         <Flex direction={'column'}>
-                           <Text h5>Your Title</Text>
+                           <Text h5>LinkUp</Text>
                            <Text span>
-                              Lorem ipsum dolor sit amet, consectetur adipiscing
-                              elit. Sed condimentum, nisl ut aliquam lacinia,
-                              nisl nisl aliquet nisl, nec
+                              LinkUp is a relationship based social media, ensuring a fun and interactive media portal for a couple friends. It features RTC for voice and video with media storage.
                            </Text>
                         </Flex>
                      </Flex>
@@ -92,11 +90,9 @@ export const Features3 = () => {
                      <Flex css={{gap: '0.5rem'}}>
                         <BoxIcon />
                         <Flex direction={'column'}>
-                           <Text h5>Your Title</Text>
+                           <Text h5>Hundred Studio's CMS</Text>
                            <Text span>
-                              Lorem ipsum dolor sit amet, consectetur adipiscing
-                              elit. Sed condimentum, nisl ut aliquam lacinia,
-                              nisl nisl aliquet nisl, nec
+                              Hundred Studio CMS is a content management system which provides real time updates to the project that is being worked on Hundered Studios.
                            </Text>
                         </Flex>
                      </Flex>
@@ -107,11 +103,9 @@ export const Features3 = () => {
                      <Flex css={{gap: '0.5rem'}}>
                         <BoxIcon />
                         <Flex direction={'column'}>
-                           <Text h5>Your Title</Text>
+                           <Text h5>Hundred Studio's Blog</Text>
                            <Text span>
-                              Lorem ipsum dolor sit amet, consectetur adipiscing
-                              elit. Sed condimentum, nisl ut aliquam lacinia,
-                              nisl nisl aliquet nisl, nec
+                              This is a interactive blog written by Hundred Studios team being published on a regular basis. This featues infotainment blogs.
                            </Text>
                         </Flex>
                      </Flex>
